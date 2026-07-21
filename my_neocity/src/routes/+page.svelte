380272dad55f753a200a194725dbde80d80696fc
GintoBos <114522869+GintoBos@users.noreply.github.com>
2026-07-21 19:22:14 +0200

    getting ready for the homepage and also fixing the welcome page enter button

--- my_neocity/src/routes/+page.svelte
+++ my_neocity/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!--this is the welcome page-->
 <!DOCTYPE html>
 <script lang="ts">
-    import '$lib/styles/home_page.css';
+    import '$lib/styles/welcome_page.css';
 </script>
 <html lang="en">
 <head>
@@ -9,54 +9,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Den's Web Page</title>
 </head>
-<body>
+    <body>
+        <div class="welcome_bg">
+            <div class="welcome">
+                <div id="welcomeContainer">
+                <img src="/images/welcome_page_images/welcomepage_textbox.png" alt="Welcome to my web page">
+                <p id="welcomeText">WELCOME TO THE DEPTHS</p>
+                <p id="welcomeText2">By DenDev712</p>
+                </div>
+            </div>
+            
+            <div id="enterButton">
+                <a href="/homepage">
+                        <img src="/images/welcome_page_images/welcomepage_enter.png" alt="Enter Button">
+                        <span>ENTER</span>
+                </a>
+            </div>
 
-    <div class="welcome">
-        <div id="welcomeContainer">
-         <img src="/images/welcome_page_images/welcomepage_textbox.png" alt="Welcome to my web page">
-         <p id="welcomeText">WELCOME TO THE DEPTHS</p>
-         <p id="welcomeText2">By DenDev712</p>
-        </div>
-    </div>
-    
-    <div id="enterButton">
-        <a href="/home">
-            <button type="button"> 
-                <img src="/images/welcome_page_images/welcomepage_enter.png" alt="Enter Button">
-                <span>ENTER</span>
-            </button>
-        </a>
-        
-    </div>
+            <div class="corals">
+                <div id="coral1">
+                    <img src="/images/welcome_page_images/coral1.png" alt="coral">
+                </div>
+                <div id="coral2">
+                    <img src="/images/welcome_page_images/coral1.png" alt="coral">
+                </div>
+                <div id="coral3">
+                    <img src="/images/welcome_page_images/coral1.png" alt="coral">  
+                </div>
+                <div id="coral4">
+                    <img src="/images/welcome_page_images/coral1.png" alt="coral">          
+                </div>
+            </div>
+            
+            <div id="octopus">
+                <img src="/images/welcome_page_images/octopus.png" alt="octopus">
+            </div>
 
-    <div class="corals">
-        <div id="coral1">
-            <img src="/images/welcome_page_images/coral1.png" alt="coral">
-        </div>
-        <div id="coral2">
-            <img src="/images/welcome_page_images/coral1.png" alt="coral">
-        </div>
-        <div id="coral3">
-            <img src="/images/welcome_page_images/coral1.png" alt="coral">  
-        </div>
-        <div id="coral4">
-            <img src="/images/welcome_page_images/coral1.png" alt="coral">          
-        </div>
-    </div>
-    
-    <div id="octopus">
-        <img src="/images/welcome_page_images/octopus.png" alt="octopus">
-    </div>
+            <div id="fish">
+                <img src="/images/welcome_page_images/fish.png" alt="fish swimming">
+            </div>
 
-    <div id="fish">
-        <img src="/images/welcome_page_images/fish.png" alt="fish swimming">
-    </div>
-
-    <div id="horsefish">
-        <img src="/images/welcome_page_images/horsefish.png" alt="horse fish swimming">
-    </div>
-    <div id="bubbles">
-        <img src="/images/welcome_page_images/bubbles.png" alt="bubbles">
-    </div>
-</body>
+            <div id="horsefish">
+                <img src="/images/welcome_page_images/horsefish.png" alt="horse fish swimming">
+            </div>
+            <div id="bubbles">
+                <img src="/images/welcome_page_images/bubbles.png" alt="bubbles">
+            </div>
+        </div>
+    </body>
 </html>
