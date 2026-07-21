e888cef53fb586500f67b702904289a3af7ef132
GintoBos <114522869+GintoBos@users.noreply.github.com>
2026-07-21 22:03:50 +0200

    Added a ugly ahh paragraph, and some stamps, that's it for today

---
+++ my_neocity/src/lib/data/helloText.ts
@@ -0,0 +1,15 @@
+export const helloText = `
+hello diver and welcome to my personal website!!
+
+i am den, a swe with a lot of hobbies,
+
+i was bored one day and i came across a video talking about indie web and the wave of nostalgia hit me,
+
+these websites have so much soul and creativity embedded on them,
+
+coding should not be seen as just a tool used by big corporates but as a tool for expressing yourself
+
+and it's not hard to create a silly website like this :3
+
+u need just a lil bit of whimsy and creativity
+`;
--- my_neocity/src/routes/homepage/+page.svelte
+++ my_neocity/src/routes/homepage/+page.svelte
@@ -1,10 +1,13 @@
 <!--homepage-->
 <script lang="ts">
+	import { helloText } from '$lib/data/helloText';
     import '$lib/styles/home_page.css';
 </script>
 
 <div class="homePage_bg"></div>
-
+<div id="bubbles">
+    <img src="/images/home_page_images/flluska.gif" alt="bubbles" > 
+</div>
 <div id="homeContainer"></div>
 <div id="homeHeader">
     <img src="/images/home_page_images/mainp_header.gif" alt="the homepage header">
@@ -13,4 +16,14 @@
     <a href="/mepage"><span id="meText">ME</span></a>
     <a href="/homepage"><span id="homeText">HOME</span></a>   
     <a href="/foryoupage"><span id="foryouText">FOR YOU</span></a>
+</div>
+
+<div id="helloTextContainer">
+    <p>{helloText}</p>
+</div>
+
+<div id="thisUserImages">
+    <img src="/images/home_page_images/this_user_likes_sky.jpg" alt="this user like night sky" id="userNightSkyImage">
+    <img src="/images/home_page_images/this_user_likes_ocean.jpg" alt="this user like ocean" id="userOceanImage">
+    <img src="/images/home_page_images/this_user_likes_miku.jpg" alt="this user like miku" id="userMikuImage">
 </div>
