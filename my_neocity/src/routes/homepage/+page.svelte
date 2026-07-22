3c428653fdb25288c19847feb35c5f7b362a8850
GintoBos <114522869+GintoBos@users.noreply.github.com>
2026-07-22 22:10:36 +0200

    Make the objects not change position when the window is resized,also added a cool ahh moon widget :3

--- my_neocity/src/routes/homepage/+page.svelte
+++ my_neocity/src/routes/homepage/+page.svelte
@@ -5,11 +5,9 @@
 </script>
 
 <div class="homePage_bg"></div>
-<div id="bubbles">
-    <img src="/images/home_page_images/flluska.gif" alt="bubbles" > 
-</div>
-<div id="homeContainer"></div>
-<div id="homeHeader">
+
+<div id="homeContainer">
+    <div id="homeHeader">
     <img src="/images/home_page_images/mainp_header.gif" alt="the homepage header">
 </div>
 <div id="meBar">
@@ -26,4 +24,56 @@
     <img src="/images/home_page_images/this_user_likes_sky.jpg" alt="this user like night sky" id="userNightSkyImage">
     <img src="/images/home_page_images/this_user_likes_ocean.jpg" alt="this user like ocean" id="userOceanImage">
     <img src="/images/home_page_images/this_user_likes_miku.jpg" alt="this user like miku" id="userMikuImage">
+</div>
+
+<div id="moonWidget">
+    <table cellpadding="0" cellspacing="0" border="0" width="212">
+    <tbody>
+        <tr>
+            <td align="center">
+                <a href="https://www.moonconnection.com" target="mc_moon_ph">
+                    <img
+                        src="https://www.moonmodule.com/cs/dm/hn.gif"
+                        alt="Moon phases"
+                    />
+                </a>
+
+                <div style="position: relative; width: 128px;">
+                    <div
+                        style="
+                            position: absolute;
+                            top: -20px;
+                            left: 6px;
+                            background: #000000;
+                            width: 116px;
+                            text-align: center;
+                        "
+                    >
+                        <a
+                            href="https://www.moonconnection.com/moon_phases.phtml"
+                            target="mc_moon_ph"
+                        >
+                            <font
+                                color="#7F7F7F"
+                                size="1"
+                                face="arial,helvetica,sans-serif"
+                            >
+                                <span
+                                    style="
+                                        color: #7F7F7F;
+                                        font-family: arial, helvetica, sans-serif;
+                                        font-size: 10px;
+                                    "
+                                >
+                                    moon phases
+                                </span>
+                            </font>
+                        </a>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+</div>
 </div>
