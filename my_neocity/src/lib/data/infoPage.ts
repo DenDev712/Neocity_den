f35d6134e18ba591b3dc4266bab20211a4d6df9d
GintoBos <114522869+GintoBos@users.noreply.github.com>
2026-08-02 21:37:48 +0200

    trying...

--- my_neocity/src/lib/data/infoPage.ts
+++ my_neocity/src/lib/data/infoPage.ts
@@ -1,6 +1,8 @@
+import { base } from '$app/paths';
+
 export const infoPage = `about this web page:
 tbh the design was the hardest and the most fun part of this project
 i recommend just brainstorming in pinterest and looking at several pages like this to create an idea/vibe
 i went with the sea and the whimsical depths vibe 
 but i have other ideas so i might manifest them as websites as well later
-if u want tools for tips and resources u can find them on <a href="/foryoupage">FOR YOU</a> section`
+if u want tools for tips and resources u can find them on <a href="${base}/foryoupage">FOR YOU</a> section`
